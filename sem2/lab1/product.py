c7a627df29ea193a9144a88468822de33d46d9e5
Maskim <maxleon1611@gmail.com>
2025-04-17 02:59:11 +0300

    delete STATUS and related methods

--- sem2/lab1/product.py
+++ sem2/lab1/product.py
@@ -1,61 +1,38 @@
-import json
-from typing import Any, Dict
 from dataclasses import dataclass, asdict
-from enum import Enum
-
-class MarketplaceError(Exception):
-    pass
+from typing import Any, Dict
+from utils import MarketplaceError
 
 @dataclass
 class Product:
-    class Status(Enum):
-        NEW = 'new'
-        SOLD = 'sold'
-        ON_SALE = 'on_sale'
-
     product_id: int
     name: str
     description: str
     price: float
-    status: Status = Status.NEW
 
     def __post_init__(self):
         if self.product_id <= 0:
-            raise MarketplaceError(f'product_id must be positive. Got: {self.product_id}')
-        if self.price >2000:
-            raise MarketplaceError('Product price must be less than 2000. Got: {self.price}')
+            raise MarketplaceError(f'Идентификатор продукта должен быть положительным. Получено: {self.product_id}')
+        if self.price > 2000:
+            raise MarketplaceError(f'Цена продукта должна быть меньше 2000. Получено: {self.price}')
         self._validate_price(self.price)
 
     def _validate_price(self, price: float) -> None:
         if price < 0:
-            raise MarketplaceError(f'Price cannot be negative. Got: {price}')
+            raise MarketplaceError(f'Цена не может быть отрицательной. Получено: {price}')
 
     def update_price(self, new_price: float) -> None:
         self._validate_price(new_price)
         self.price = new_price
 
-    def mark_as_sold(self) -> None:
-        self.status = self.Status.SOLD
-
-    def mark_as_on_sale(self) -> None:
-        self.status = self.Status.ON_SALE
-
     def to_dict(self) -> Dict[str, Any]:
-        data = asdict(self)
-        data['status'] = self.status.value
-        return data
+        return asdict(self)
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> 'Product':
-        try:
-            if 'status' in data:
-                data['status'] = cls.Status(data['status'])
-            return cls(**data)
-        except ValueError as e:
-            raise MarketplaceError(f'Invalid status: {data.get("status")}') from e
+        return cls(**data)
 
     def __str__(self) -> str:
-        return f'{self.name} (${self.price}, status: {self.status.value})'
+        return f'{self.name} (${self.price})'
 
     def __eq__(self, other):
         if isinstance(other, Product):
@@ -64,4 +41,3 @@ class Product:
 
     def __hash__(self):
         return hash(self.product_id)
-

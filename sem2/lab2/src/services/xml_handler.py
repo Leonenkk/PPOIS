d0bcc2026072637cb65cdc8a4d0c03d6d0d7adeb
Leonenkk <maxleon1611@gmail.com>
2025-04-29 02:59:23 +0300

    export_to_xml method

--- sem2/lab2/src/services/xml_handler.py
+++ sem2/lab2/src/services/xml_handler.py
@@ -35,3 +35,33 @@ class XMLHandler:
             print("Data imported from XML successfully.")
         except Exception as e:
             print(f"Error importing from XML: {e}")
+
+    def export_to_xml(self, xml_file_path: str, players: list[Player] = None):
+        try:
+            if not xml_file_path.endswith(".xml"):
+                xml_file_path += ".xml"
+
+            if players is None:
+                players = self.db_repository.get_players()
+
+            root = ET.Element("players")
+
+            for player in players:
+                player_element = ET.SubElement(root, "player")
+
+                ET.SubElement(player_element, "full_name").text = player.full_name
+                ET.SubElement(player_element, "birth_date").text = player.birth_date.isoformat()
+                ET.SubElement(player_element, "team").text = player.team
+                ET.SubElement(player_element, "home_city").text = player.home_city
+                ET.SubElement(player_element, "squad").text = player.squad
+                ET.SubElement(player_element, "position").text = player.position
+
+            tree = ET.ElementTree(root)
+            tree.write(xml_file_path, encoding="utf-8", xml_declaration=True)
+
+            print(f"Data exported to XML successfully: {xml_file_path}")
+        except Exception as e:
+            print(f"Error exporting to XML: {e}")
+
+    def export_selected_to_xml(self, xml_file_path: str, selected_players: list[Player]):
+        self.export_to_xml(xml_file_path, selected_players)
